ec4b2d4b23e60473d175d4b99e143de59b67b36e
dariossneto <dariosnneto@gmail.com>
2026-03-07 00:42:38 -0300

    Add comments

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,14 +3,14 @@ import { test, expect } from '@playwright/test';
 /// AAA - Arrange, Act, Assert
 /// PAV - Preparar, Agir, Verificar
 
-
-
 test('should check an approved order', async ({ page }) => {
     // Arrange
     await page.goto('http://localhost:5173/');
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+    // Navigation
     await page.getByRole('link', { name: 'Consultar Pedido' }).click();
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+    
     // Act
     await page.getByTestId('search-order-id').fill('VLO-NE93JO');
     await page.getByTestId('search-order-button').click();
