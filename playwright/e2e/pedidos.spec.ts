679a7a9a8ebbf0fd1e5a1c243267ee0ef4f3df21
dariossneto <dariosnneto@gmail.com>
2026-04-13 02:04:11 -0300

    Atualização main

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,24 +1,54 @@
 import { test, expect } from '@playwright/test';
 
+import { generateOrderCode } from '../support/helpers';
+
 /// AAA - Arrange, Act, Assert
 /// PAV - Preparar, Agir, Verificar
-
-test('should check an approved order', async ({ page }) => {
-    // Arrange
-    await page.goto('http://localhost:5173/');
-    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-    // Navigation
-    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-    await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+test.describe('Order Inquiry', () => {
     
-    // Act
-    await page.getByTestId('order-id-input').fill('VLO-NE93JO');
-    await page.getByTestId('search-order-button').click();
+    test.beforeEach(async ({ page }) => {
+        //Arrange
+        await page.goto('http://localhost:5173/');
+        await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+        // Navigation
+        await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+        await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
-    // Assert
-    await expect(page.getByText('PedidoVLO-NE93JOAPROVADO')).toBeVisible({timeout: 10_000});
-    await expect(page.getByTestId('order-result-id')).toContainText('VLO-NE93JO');
+    });
 
-    await expect(page.getByTestId('order-result-status')).toBeVisible();
-    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
-});
+    test('should check an approved order', async ({ page }) => {
+        
+        // Test Data
+        const order = 'VLO-NE93JO'
+
+        // Act
+        await page.getByTestId('order-id-input').fill(order);
+        await page.getByTestId('search-order-button').click();
+
+        // Assert
+        const containerPedido = page.getByRole('paragraph')
+        .filter({ hasText: /^Pedido$/ })
+        .locator('..')
+
+        await expect(containerPedido).toContainText(order, {timeout: 10_000})
+        await expect(page.getByText('APROVADO')).toBeVisible()
+    });
+
+    test('a message should be displayed when the order is not found', async ({ page }) => {
+
+        const order = generateOrderCode();
+
+
+        // Act
+        await page.getByTestId('order-id-input').fill(order);
+        await page.getByTestId('search-order-button').click();
+        
+
+        await expect(page.locator('#root')).toMatchAriaSnapshot(`
+            - img
+            - heading "Pedido não encontrado" [level=3]
+            - paragraph: Verifique o número do pedido e tente novamente
+            `);
+
+    });
+})
